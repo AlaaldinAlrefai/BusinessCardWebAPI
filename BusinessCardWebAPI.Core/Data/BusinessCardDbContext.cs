b534f980348faef31e66ab1139657d2ca1de025c
alaa alrefai <alaaalrefai661@gmail.com>
2024-10-04 02:42:23 -0700

    settings

---
+++ BusinessCardWebAPI.Core/Data/BusinessCardDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCardWebAPI.Core.Data
+{
+    internal class DbContext
+    {
+    }
+}
